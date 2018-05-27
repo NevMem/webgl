efb51e0720013422148d81a6b61847e2ec172ffa
NevMem <memlolkek@gmail.com>
2018-05-27 21:09:15 +0300

    GPGPU
    Fixed all bugs in matrix multiplication

--- resources/fShader.glsl
+++ resources/fShader.glsl
@@ -7,23 +7,23 @@ uniform sampler2D tex1;
 uniform sampler2D tex2;
 out vec4 outColor;
 
-int tr(vec4 a){
-	return int(a.x * 255.) * 16777216 + int(a.y * 255.) * 65536 + int(a.z * 255.) * 255 + int(a.w * 255.);
+uint tr(vec4 a){
+	return uint(a.x * 255.) * uint(16777216) + uint(a.y * 255.) * uint(65536) + uint(a.z * 255.) * uint(256) + uint(a.w * 255.);
 }
 
 void main() {
-	int res = 0;
-	int n = 16;
+	uint res = uint(0);
+	int n = 128;
 	for(int i = 0;i < n;i++) {
 		res += 
 			tr(texture(tex1, vec2(float(2 * n - 2 * i - 1) / float(2 * n), tCoord.x))) 
 			* tr(texture(tex2, vec2(tCoord.y, float(2 * n - 2 * i - 1) / float(2 * n))));
 	}
-	int red = res / 16777216;
-	res -= red * 16777216;
-	int green = res / 65536;
-	res -= green * 65536;
-	int blue = res / 256;
-	res -= blue * 256;
+	uint red = res / uint(16777216);
+	res -= red * uint(16777216);
+	uint green = res / uint(65536);
+	res -= green * uint(65536);
+	uint blue = res / uint(256);
+	res -= blue * uint(256);
 	outColor = vec4(float(red) / 255., float(green) / 255., float(blue) / 255., float(res) / 255.);
 }
